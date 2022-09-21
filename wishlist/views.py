a86dfb66bcafc66bd62c6f26be196e7580d65c78
jasonirvine76 <jason.irvine@ui.ac.id>
2022-09-21 23:01:20 +0700

    merapihkan import

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+import datetime
+
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.core import serializers
+from django.urls import reverse
 from django.shortcuts import redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-import datetime
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 
 from wishlist.models import BarangWishList
 
