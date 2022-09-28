5644f94fc2b064443bc6c33db37281e597e8b19f
jasonirvine76 <jason.irvine@ui.ac.id>
2022-09-29 00:12:23 +0700

    feat: add bootstrap

--- project_django/settings.py
+++ project_django/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_URL = 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
