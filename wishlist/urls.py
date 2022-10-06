61f27904be0ee135f935dbd7588db890d2587653
jasonirvine76 <jason.irvine@ui.ac.id>
2022-10-06 14:21:54 +0700

    feat: tugas 4

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from wishlist.views import login_user, show_json, show_json_by_id, show_wishlist, show_xml, register, login_user, logout_user
+from wishlist.views import show_wishlist_ajax, login_user, show_json, show_json_by_id, show_wishlist, show_xml, register, login_user, logout_user
 
 
 app_name = 'wishlist'
@@ -11,5 +11,6 @@ urlpatterns = [
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('wishlist-ajax', show_wishlist_ajax, name='wishlist-ajax')
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -25,6 +25,16 @@ def show_wishlist(request):
     }
     return render(request, "wishlist.html", context)
 
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishList.objects.all()
+    context = {
+        'list_barang' : data_barang_wishlist,
+        'nama' : 'Jason Irvine M.P',
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "wishlist_ajax.html", context)
+
 def show_xml(request):
     data = BarangWishList.objects.all()
     return HttpResponse(serializers.serialize('xml', data), content_type = "application/xml")
@@ -46,7 +56,8 @@ def register(request):
             form.save()
             messages.success(request, 'Akun telah berhasil dibuat!')
             return redirect('wishlist:login')
-
+        else:
+            messages.success(request, 'Akun tidak dapat dibuat')
     context = {'form':form}
     return render(request, 'register.html', context)
 
